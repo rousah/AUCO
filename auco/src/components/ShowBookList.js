9dc7a38abf52c553f512ee619fa9734e3b041271
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-03-10 20:06:37 +0100

    Everything is connected - first test

---
+++ auco/src/components/ShowBookList.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class ShowBookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:8082/api/books')
+      .then(res => {
+        this.setState({
+          books: res.data
+        })
+      })
+      .catch(err =>{
+        console.log('Error from ShowBookList');
+      })
+  };
+
+
+  render() {
+    const books = this.state.books;
+    console.log("PrintBook: " + books);
+    let bookList;
+
+    return (
+      <div className="ShowBookList">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Books List</h2>
+            </div>
+
+            <div className="col-md-11">
+              <Link to="/create-book" className="btn btn-outline-warning float-right">
+                + Add New Book
+              </Link>
+              <br />
+              <br />
+              <hr />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShowBookList;
