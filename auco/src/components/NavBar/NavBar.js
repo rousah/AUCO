4fb00cf5d739e2930e0c8918e1ec7d3a8354252e
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-04-01 11:02:37 +0200

    log in and register button on navbar

--- auco/src/components/NavBar/NavBar.js
+++ auco/src/components/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
 import './NavBar.css';
+import ButtonMain from '../ButtonMain';
 import {
     Collapse,
     Navbar,
     NavbarToggler,
     NavbarBrand,
     Nav,
+    NavItem,
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
@@ -21,20 +23,20 @@ const NavBar = (props) => {
 
     const changeBackground = () => {
         console.log(window.scrollY);
-        if(window.scrollY >= 10) {
+        if (window.scrollY >= 10) {
             setNavbar(true);
-        } else setNavbar(false); 
+        } else setNavbar(false);
     }
 
     window.addEventListener('scroll', changeBackground);
 
     return (
         <Navbar color="light" dark expand="md" className={navbar ? 'sticky-top navigation active' : 'sticky-top navigation'}>
-            <NavbarBrand href="/" className="logo"><h1 style={{fontWeight:"800"}}>AUCO</h1></NavbarBrand>
+            <NavbarBrand href="/" className="logo"><h1 style={{ fontWeight: "800" }}>AUCO</h1></NavbarBrand>
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
                 <Nav className="ms-auto" navbar>
-                    <UncontrolledDropdown nav inNavbar style={{fontSize: "20px", fontWeight: "600"}}>
+                    <UncontrolledDropdown nav inNavbar style={{ fontSize: "20px", fontWeight: "600" }}>
                         <DropdownToggle nav caret>
                             Español
                         </DropdownToggle>
@@ -48,6 +50,12 @@ const NavBar = (props) => {
                             </DropdownItem>
                         </DropdownMenu>
                     </UncontrolledDropdown>
+                    <NavItem className={navbar ? "" : "hidden"}>
+                        <ButtonMain className="px-3 mx-1" buttonText={"REGISTRAR"} href="/register" fontWeight="500" fontSize="20px"></ButtonMain>
+                    </NavItem>
+                    <NavItem className={navbar ? "" : "hidden"}>
+                        <ButtonMain secondary className="px-3 mx-1" buttonText={"INICIAR SESIÓN"} href="/login" fontWeight="500" fontSize="20px"></ButtonMain>
+                    </NavItem>
                 </Nav>
             </Collapse>
         </Navbar>
