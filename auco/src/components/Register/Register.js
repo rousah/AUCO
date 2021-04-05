57a1a78fe2b02d0b2d56b52d362e2e84e8a1c0f3
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-04-05 19:01:23 +0200

    Finish choose user type page

--- auco/src/components/Register/Register.js
+++ auco/src/components/Register/Register.js
@@ -2,16 +2,33 @@ import React from 'react';
 import '../../App.css';
 import './Register.css';
 import NavBar from '../NavBar/NavBar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import childrenIllustration from '../../assets/illustrations/round.png';
+import teacher from '../../assets/illustrations/teacher.png';
+import student from '../../assets/illustrations/student.png';
 import { Container, Row, Col } from 'reactstrap';
-import ButtonMain from './../ButtonMain';
+import SquareButton from '../Buttons/SquareButton';
 
 const Register = (props) => {
 
     return (
-        <div className="register">
-            <NavBar></NavBar>
+        <div className="register fullscreen">
+            <NavBar showlogin></NavBar>
+            <div className="d-flex h-100 align-items-center">
+                <Container className="align-content-center">
+                    <Row className="align-content-center text-center mb-5">
+                        <Col>
+                            <h2 style={{ fontWeight: 800, color: "#3956f7" }}>Soy un/a...</h2>
+                        </Col>
+                    </Row>
+                    <Row className="mb-5">
+                        <Col className="align-items-center justify-content-center d-flex">
+                            <SquareButton href="/create-account" buttonText="Profesor/a" secondary illustration={teacher} alt="teacher" width="30%"></SquareButton>
+                        </Col>
+                        <Col className="align-items-center justify-content-center d-flex">
+                            <SquareButton buttonText="Alumno/a" secondary illustration={student} alt="student" width="30%"></SquareButton>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
