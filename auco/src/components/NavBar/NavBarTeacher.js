425971bd77adeaf2c1c156d20ced35309ada67cb
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-04-13 13:17:42 +0200

    navbar design

--- auco/src/components/NavBar/NavBarTeacher.js
+++ auco/src/components/NavBar/NavBarTeacher.js
@@ -6,65 +6,61 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUsers, faBell, faCog, faUser, faQuestion, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
     NavLink,
     Container,
-    FormFeedback
 } from 'reactstrap';
 
 const NavBarLanding = (props) => {
 
     return (
-        <header class="px-3 py-2 bg-dark text-white">
+        <header class="px-3 pt-1 navbar-teacher text-white">
             <Container>
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <NavbarBrand href="/" className="logo me-lg-auto"><h1 style={{ fontWeight: "800" }} className="m-0">AUCO</h1></NavbarBrand>
                     <Nav className="col-12 col-lg-auto my-2 justify-content-center my-md-0">
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faHome} className="bi d-block mx-auto mb-1" />
+                            <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faHome} className="bi d-block mx-auto mb-1" size="lg" />
                                 Home
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faUsers} className="bi d-block mx-auto mb-1" />
+                            <NavLink href="" active className="py-0">
+                                <FontAwesomeIcon icon={faUsers} className="bi d-block mx-auto mb-1" size="lg" />
                                 Clases
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faBell} className="bi d-block mx-auto mb-1" />
+                        <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faBell} className="bi d-block mx-auto mb-1" size="lg" />
                                 Avisos
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faCog} className="bi d-block mx-auto mb-1" />
+                        <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faCog} className="bi d-block mx-auto mb-1" size="lg" />
                                 Ajustes
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faUser} className="bi d-block mx-auto mb-1" />
+                        <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faUser} className="bi d-block mx-auto mb-1" size="lg" />
                                 Perfil
                             </NavLink>
                         </NavItem>
-                        <span id="vertical-separation"></span>
+                        <span id="vertical-separation" className="mt-1 mb-2"></span>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faQuestion} className="bi d-block mx-auto mb-1" />
+                        <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faQuestion} className="bi d-block mx-auto mb-1" size="lg" />
                                 Ayuda
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="">
-                                <FontAwesomeIcon icon={faSignOutAlt} className="bi d-block mx-auto mb-1" />
+                            <NavLink href="" className="py-0">
+                                <FontAwesomeIcon icon={faSignOutAlt} className="bi d-block mx-auto mb-1" size="lg" />
                                 Salir
                             </NavLink>
                         </NavItem>
