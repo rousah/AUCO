09ea3ee71380a5aab0f3332cc1955de21de2f829
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-04-15 15:25:28 +0200

    Change selected according to active item in nav

--- auco/src/components/NavBar/NavBarTeacher.js
+++ auco/src/components/NavBar/NavBarTeacher.js
@@ -20,44 +20,44 @@ const NavBarLanding = (props) => {
                     <NavbarBrand href="/" className="logo me-lg-auto"><h1 style={{ fontWeight: "800" }} className="m-0">AUCO</h1></NavbarBrand>
                     <Nav className="col-12 col-lg-auto my-2 justify-content-center my-md-0">
                         <NavItem>
-                            <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.home ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faHome} className="bi d-block mx-auto mb-1" size="lg" />
                                 Home
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="" active className="py-0 teacher">
+                            <NavLink href="" className={props.clases ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faUsers} className="bi d-block mx-auto mb-1" size="lg" />
                                 Clases
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                        <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.notifications ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faBell} className="bi d-block mx-auto mb-1" size="lg" />
                                 Avisos
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                        <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.settings ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faCog} className="bi d-block mx-auto mb-1" size="lg" />
                                 Ajustes
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                        <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.profile ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faUser} className="bi d-block mx-auto mb-1" size="lg" />
                                 Perfil
                             </NavLink>
                         </NavItem>
                         <span id="vertical-separation" className="mt-1 mb-2"></span>
                         <NavItem>
-                        <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.help ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faQuestion} className="bi d-block mx-auto mb-1" size="lg" />
                                 Ayuda
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="" className="py-0 teacher">
+                            <NavLink href="" className={props.logout ? "py-0 teacher active" : "py-0 teacher "}>
                                 <FontAwesomeIcon icon={faSignOutAlt} className="bi d-block mx-auto mb-1" size="lg" />
                                 Salir
                             </NavLink>
