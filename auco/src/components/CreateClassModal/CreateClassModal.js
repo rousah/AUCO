492a228b6cbf78811537d0da21fbf49ba59ddd06
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-05-24 21:52:40 +0200

    Add loading when creating class

--- auco/src/components/CreateClassModal/CreateClassModal.js
+++ auco/src/components/CreateClassModal/CreateClassModal.js
@@ -9,6 +9,7 @@ import './CreateClassModal.css'
 import file from '../../assets/files/plantilla_alumnos.xlsx'
 import { postClass } from '../../services/createClass';
 import useToken from '../../services/useToken';
+import Loading from '../../components/Loading';
 
 const initialFormData = Object.freeze({
     userId: "",
@@ -22,6 +23,7 @@ const initialFormData = Object.freeze({
 
 const CreateClassModal = (props) => {
     const { userId } = useToken();
+    const [loading, setLoading] = useState(false);
     //let history = useHistory();
 
     const [activeTab, setActiveTab] = useState('1');
@@ -96,10 +98,18 @@ const CreateClassModal = (props) => {
         data.append('students', JSON.stringify(formData.students));
         data.append('withFile', formData.withFile);
         data.append('selectedFile', formData.selectedFile);
+
+        // Show loading animation
+        setLoading(true);
+
+        // Create class
         postClass(data).then(response => {
-            // if login success
+            // if create class success
             if (response) {
                 console.log(response);
+                setLoading(false);
+                props.toggle()
+                window.location.reload();
             }
         });
     };
@@ -116,93 +126,101 @@ const CreateClassModal = (props) => {
         <Modal isOpen={props.modal} toggle={props.toggle} className={props.className} style={{ maxWidth: '75%' }}>
             <ModalHeader toggle={props.toggle} close={closeBtn}>Creando una clase</ModalHeader>
             <ModalBody style={{ minHeight: '50vh' }}>
-                <Form>
-                    <FormGroup>
-                        <Label for="classname">
-                            <h6>Nombre de la clase:</h6>
-                        </Label>
-                        <Input type="text" name="classname" className="mb-3" onChange={handleChange} placeholder="Ética" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="year">
-                            <h6>Curso:</h6>
-                        </Label>
-                        <Input type="text" name="year" className="mb-3" onChange={handleChange} placeholder="2º ESO" />
-                    </FormGroup>
-                    <h6 className="mt-5">Alumnos:</h6>
-                    <div style={styleBorder}>
-                        <p className="fs-6 p-1">
-                            Para cada alumno añadido a la clase se creará una cuenta con un usuario y contraseña para el alumno, que podrá cambiar una vez activada la cuenta iniciando sesión. De esta forma se le da acceso al contenido a los alumnos. Se pueden añadir de forma manual, escribiendo el nombre y apellido de cada uno de ellos, o subiendo una hoja de cálculo con un formato específico.
-                        </p>
-                        <Nav tabs>
-                            <NavItem>
-                                <NavLink className={classnames({ active: activeTab === '1' })}
-                                    onClick={() => { toggleTab('1'); }}>Añadir de forma manual</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className={classnames({ active: activeTab === '2' })}
-                                    onClick={() => { toggleTab('2'); }}>Añadir con hoja de cálculo</NavLink>
-                            </NavItem>
-                        </Nav>
-                        <TabContent activeTab={activeTab}>
-                            <TabPane tabId="1" className="p-2">
-                                <p className="fs-6">
-                                    Escribe los nombres y apellidos de todos los alumnos de la clase:
+                {
+                    loading ?
+                        <Loading text="Creando clase..."></Loading>
+                        :
+                        <Form>
+                            <FormGroup>
+                                <Label for="classname">
+                                    <h6>Nombre de la clase:</h6>
+                                </Label>
+                                <Input type="text" name="classname" className="mb-3" onChange={handleChange} placeholder="Ética" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="year">
+                                    <h6>Curso:</h6>
+                                </Label>
+                                <Input type="text" name="year" className="mb-3" onChange={handleChange} placeholder="2º ESO" />
+                            </FormGroup>
+                            <h6 className="mt-5">Alumnos:</h6>
+                            <div style={styleBorder}>
+                                <p className="fs-6 p-1">
+                                    Para cada alumno añadido a la clase se creará una cuenta con un usuario y contraseña para el alumno, que podrá cambiar una vez activada la cuenta iniciando sesión. De esta forma se le da acceso al contenido a los alumnos. Se pueden añadir de forma manual, escribiendo el nombre y apellido de cada uno de ellos, o subiendo una hoja de cálculo con un formato específico.
                                 </p>
-                                {
-                                    students.map((val, i) => {
-                                        return (
-                                            <Row key={i.toString()}>
-                                                <Col className="align-items-center">
-                                                    <FormGroup>
-                                                        <Input type="text" name="name" className="mb-3" onChange={handleChange} placeholder={'Nombre alumno #' + i} data-id={i} />
-                                                    </FormGroup>
-                                                </Col>
-                                                <Col className="">
-                                                    <FormGroup>
-                                                        <Input type="text" name="surname" className="mb-3" onChange={handleChange} placeholder={'Apellidos alumno #' + i} data-id={i} />
-                                                    </FormGroup>
-                                                </Col>
-                                                <Col sm="auto" className="mb-3 align-self-center ps-0">
-                                                    {i < students.length - 1 ?
-                                                        <FontAwesomeIcon icon={faPlusCircle} className="fs-5" style={{ color: '#ffffff' }}></FontAwesomeIcon>
-                                                        :
-                                                        <FontAwesomeIcon icon={faPlusCircle} className="fs-5" style={{ color: '#3956f7', cursor: 'pointer' }} onClick={addStudent}></FontAwesomeIcon>
-                                                    }
-                                                </Col>
-                                            </Row>
-                                        )
-                                    })
-                                }
-                            </TabPane>
-                            <TabPane tabId="2" className="p-2">
-                                <p className="fs-6">
-                                    Sube un archivo .xls con los datos de los alumnos de la clase formateados de la siguiente forma:
-                                </p>
-                                <p className="fs-6">
-                                    Columna A: Nombre <br></br>
-                                    Columna B: Apellido
-                                </p>
-                                <p className="fs-6">
-                                    O bien, <a download href={file}>descarga la plantilla</a>, rellénala y súbela.
-                                {/*<ButtonMain buttonText="Descargar plantilla" className="px-2 rounded-3 me-3 py-1 fs-6" fontWeight="300" download href={file}></ButtonMain>*/}
-                                </p>
-                                <FormGroup>
-                                    <Label for="exampleCustomFileBrowser">Seleccionar hoja de cálculo:</Label>
-                                    <CustomInput type="file" id="exampleCustomFileBrowser" name="customFile" label="Selecciona un archivo" onChange={onFileChange} accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
-                                    <FormText color="muted" className="ms-1">
-                                        Formatos aceptados: .xlsx .xls .csv
+                                <Nav tabs>
+                                    <NavItem>
+                                        <NavLink className={classnames({ active: activeTab === '1' })}
+                                            onClick={() => { toggleTab('1'); }}>Añadir de forma manual</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className={classnames({ active: activeTab === '2' })}
+                                            onClick={() => { toggleTab('2'); }}>Añadir con hoja de cálculo</NavLink>
+                                    </NavItem>
+                                </Nav>
+                                <TabContent activeTab={activeTab}>
+                                    <TabPane tabId="1" className="p-2">
+                                        <p className="fs-6">
+                                            Escribe los nombres y apellidos de todos los alumnos de la clase:
+                                        </p>
+                                        {
+                                            students.map((val, i) => {
+                                                return (
+                                                    <Row key={i.toString()}>
+                                                        <Col className="align-items-center">
+                                                            <FormGroup>
+                                                                <Input type="text" name="name" className="mb-3" onChange={handleChange} placeholder={'Nombre alumno #' + i} data-id={i} />
+                                                            </FormGroup>
+                                                        </Col>
+                                                        <Col className="">
+                                                            <FormGroup>
+                                                                <Input type="text" name="surname" className="mb-3" onChange={handleChange} placeholder={'Apellidos alumno #' + i} data-id={i} />
+                                                            </FormGroup>
+                                                        </Col>
+                                                        <Col sm="auto" className="mb-3 align-self-center ps-0">
+                                                            {i < students.length - 1 ?
+                                                                <FontAwesomeIcon icon={faPlusCircle} className="fs-5" style={{ color: '#ffffff' }}></FontAwesomeIcon>
+                                                                :
+                                                                <FontAwesomeIcon icon={faPlusCircle} className="fs-5" style={{ color: '#3956f7', cursor: 'pointer' }} onClick={addStudent}></FontAwesomeIcon>
+                                                            }
+                                                        </Col>
+                                                    </Row>
+                                                )
+                                            })
+                                        }
+                                    </TabPane>
+                                    <TabPane tabId="2" className="p-2">
+                                        <p className="fs-6">
+                                            Sube un archivo .xls con los datos de los alumnos de la clase formateados de la siguiente forma:
+                                        </p>
+                                        <p className="fs-6">
+                                            Columna A: Nombre <br></br>
+                                            Columna B: Apellido
+                                        </p>
+                                        <p className="fs-6">
+                                            O bien, <a download href={file}>descarga la plantilla</a>, rellénala y súbela.
+                                        </p>
+                                        <FormGroup>
+                                            <Label for="exampleCustomFileBrowser">Seleccionar hoja de cálculo:</Label>
+                                            <CustomInput type="file" id="exampleCustomFileBrowser" name="customFile" label="Selecciona un archivo" onChange={onFileChange} accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+                                            <FormText color="muted" className="ms-1">
+                                                Formatos aceptados: .xlsx .xls .csv
                                     </FormText>
-                                </FormGroup>
-                            </TabPane>
-                        </TabContent>
-                    </div>
-                </Form>
+                                        </FormGroup>
+                                    </TabPane>
+                                </TabContent>
+                            </div>
+                        </Form>
+                }
             </ModalBody>
-            <ModalFooter>
-                <ButtonMain buttonText="Crear clase" className="px-2 rounded-4 me-3 py-1" fontWeight="500" fontSize="18px" onClick={handleSubmit}></ButtonMain>
-                <ButtonMain secondary buttonText="Cancelar" className="px-2 rounded-4 py-1" fontWeight="500" fontSize="18px" onClick={props.toggle}></ButtonMain>
-            </ModalFooter>
+            {
+                loading ? <span></span>
+                    :
+                    <ModalFooter>
+                        <ButtonMain buttonText="Crear clase" className="px-2 rounded-4 me-3 py-1" fontWeight="500" fontSize="18px" onClick={handleSubmit}></ButtonMain>
+                        <ButtonMain secondary buttonText="Cancelar" className="px-2 rounded-4 py-1" fontWeight="500" fontSize="18px" onClick={props.toggle}></ButtonMain>
+                    </ModalFooter>
+            }
         </Modal >
     );
 }
