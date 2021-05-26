1749c5dac2cd7be01c7bac248058370f18ea60c9
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-05-26 13:19:14 +0200

    Progress bar card finished

--- auco/src/pages/Student/Home/StudentHome.js
+++ auco/src/pages/Student/Home/StudentHome.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import NavBarStudent from '../../../components/NavBar/NavBarStudent';
 import { Container, Row, Col } from 'reactstrap';
-import { getStudent } from '../../../services/getStudent';
 import Loading from '../../../components/Loading';
 import useToken from '../../../services/useToken';
 import DashboardCard from '../../../components/Dashboard/DashboardCard';
+import { Progress } from 'reactstrap';
+import levelIllustration from '../../../assets/illustrations/badges/levels/LEVEL1.png';
 
 const StudentHome = (props) => {
     const { currentUser } = useToken();
@@ -25,8 +26,30 @@ const StudentHome = (props) => {
                         <Row>
                             <Col>
                                 <DashboardCard title="Progreso" className="h-100" content={
-                                    <div>
-                                        Hello
+                                    <div className="d-flex">
+                                        <Col xs="1" className="me-4">
+                                            <img src={levelIllustration} alt="level badge" style={{ width: "100%" }}></img>
+                                        </Col>
+                                        <Col className="d-flex flex-column justify-content-center">
+                                            <Row>
+                                                <Col className="text-muted">
+                                                    Objetivo: Nivel 2
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col className="d-flex flex-column justify-content-center">
+                                                    <Progress value="50" color="primary" striped animated></Progress>
+                                                </Col>
+                                                <Col xs="1" className="d-flex flex-column justify-content-center">
+                                                    <span>50/100</span>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col className="invisible">
+                                                    <Progress value="50" color="primary" striped animated></Progress>
+                                                </Col>
+                                            </Row>
+                                        </Col>
                                     </div>
                                 }></DashboardCard>
                             </Col>
