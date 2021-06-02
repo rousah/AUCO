7cb796391b4f950399187f61c608051ed452257a
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-02 12:10:35 +0200

    Toast style update colour

--- auco/src/components/Notification/SuccessAlert.js
+++ auco/src/components/Notification/SuccessAlert.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Toast, ToastBody } from 'reactstrap';
 
 const SuccessAlert = (props) => {
-    // Toast
+    // Toast style background and colours
     const toastStyle = {
         position: 'absolute',
         right: '1rem',
-        backgroundColor: 'rgba(57, 86, 247, 0.7)',
-        border: '0',
+        backgroundColor: 'rgba(57, 174, 164, 0.7)',
+        borderColor: 'rgba(61, 208, 174)',
         color: 'white'
     }
 
