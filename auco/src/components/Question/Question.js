4ba773aae7f764488314ed843949b534c8ed49d7
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-08 17:38:51 +0200

    Add questions in slider

--- auco/src/components/Question/Question.js
+++ auco/src/components/Question/Question.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import ButtonMain from '../Buttons/ButtonMain';
-import { Container, Row, Col } from 'reactstrap';
 import AnswerScale from './AnswerScale';
 import Answer from './Answer';
-import DashboardCard from '../Dashboard/DashboardCard';
 import './question.css';
 
 const Question = (props) => {
     return (
-        props.scale ?
+        props.choice ?
             <div>
-                <AnswerScale question="Algún compañero/a me ha pegado o golpeado para hacerme daño de verdad"></AnswerScale>
-            </div>
-            :
-            <div>
-                <h4 className="text-center">Cuando has respondido a las preguntas anteriores, ¿en quién estabas pensando?</h4>
+                <h4 className="text-center">{props.question}</h4>
                 <Answer text="En un compañero especialmente"></Answer>
                 <Answer text="En una compañera especialmente"></Answer>
                 <Answer text="En un compañero especialmente, acompañado de un grupo de amigos/as que le apoyan"></Answer>
                 <Answer text="En una compañera especialmente, acompañada de un grupo de amigos/as que le apoyan"></Answer>
                 <Answer text="En varios compañeros/as que son un grupo de amigos"></Answer>
+            </div>
+            :
+            <div>
+                <AnswerScale question={props.question} answers={props.answers}></AnswerScale>
             </div>
     );
 }
--- auco/src/pages/Student/Questionnaires/Questionnaires.js
+++ auco/src/pages/Student/Questionnaires/Questionnaires.js
@@ -8,6 +8,8 @@ import Question from '../../../components/Question/Question';
 import ButtonMain from '../../../components/Buttons/ButtonMain';
 import { getQuestionnaire } from '../../../services/getQuestionnaire';
 import { NavLink as RRNavLink, Link } from 'react-router-dom';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import './questionnaires.css';
 
 const Questionnaires = (props) => {
@@ -15,6 +17,16 @@ const Questionnaires = (props) => {
     let { questionnaireId } = useParams();
     const { currentUser } = useToken();
     console.log(questionnaireId);
+
+    const noButtonStyle = {
+        background: "none",
+        color: "inherit",
+        border: "none",
+        padding: "0",
+        font: "inherit",
+        cursor: "default",
+        outline: "inherit"
+    }
 
     useEffect(() => {
         const getMyQuestionnaire = async (id) => {
@@ -37,37 +49,51 @@ const Questionnaires = (props) => {
                 questionnaire ?
                     <div className="fullscreen justify-content-between d-flex flex-column">
                         {console.log(questionnaire)}
-                        <Container className="w-50 mt-3">
-                            <div>
-                                <Row>
-                                    <h2>{questionnaire.name}</h2>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <Progress value="10" color="secondary"></Progress>
-                                    </Col>
-                                </Row>
-                                <Row className="mb-5 p-3">
-                                    {questionnaire.description}
-                                </Row>
-                                <Row className="mt-3">
-                                    {
-                                        questionnaire.name == "Bullying" ?
-                                            <Question scale></Question>
-                                            :
-                                            <Question></Question>
-                                    }
-                                </Row>
+                        <CarouselProvider
+                            naturalSlideWidth={100}
+                            naturalSlideHeight={60}
+                            totalSlides={questionnaire.questions.length}>
+                            <Container className="w-50 mt-3">
+                                <div>
+                                    <Row>
+                                        <h2>{questionnaire.name}</h2>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <Progress value="10" color="secondary"></Progress>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-5 p-3">
+                                        {questionnaire.description}
+                                    </Row>
+                                    <Row className="mt-3">
+                                        <Slider>
+                                            {
+                                                questionnaire.questions.map((val, i) => {
+                                                    return (
+                                                        <Slide index={i}>
+                                                            <Question choice={val.type} answers={val.answers} question={val.question}></Question>
+                                                        </Slide>
+                                                    )
+                                                })
+                                            }
+                                        </Slider>
+                                    </Row>
+                                </div>
+                            </Container>
+                            <div className="border-top">
+                                <div className="w-50 container d-flex justify-content-between">
+                                    <ButtonBack style={noButtonStyle}>
+                                        <Link tag={RRNavLink} to={{ pathname: "/home" }} style={{ textDecoration: "none" }}>
+                                            <ButtonMain secondary buttonText="Volver" className="py-2 px-3 mt-4" fontWeight="600" fontSize="20px"></ButtonMain>
+                                        </Link>
+                                    </ButtonBack>
+                                    <ButtonNext style={noButtonStyle}>
+                                        <ButtonMain buttonText="Siguiente" className="py-2 px-3 mt-4" fontWeight="600" fontSize="20px"></ButtonMain>
+                                    </ButtonNext>
+                                </div>
                             </div>
-                        </Container>
-                        <div className="border-top">
-                            <div className="w-50 container d-flex justify-content-between">
-                                <Link tag={RRNavLink} to={{ pathname: "/home" }} style={{textDecoration: "none"}}>
-                                    <ButtonMain secondary buttonText="Volver" className="py-2 px-3 mt-4" fontWeight="600" fontSize="20px" onClick={""}></ButtonMain>
-                                </Link>
-                                <ButtonMain buttonText="Siguiente" className="py-2 px-3 mt-4" fontWeight="600" fontSize="20px" onClick={""}></ButtonMain>
-                            </div>
-                        </div>
+                        </CarouselProvider>
                     </div>
                     :
                     <Loading></Loading>
