01735abd95bbbea4b5ac26177a63f91c32df2bd1
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-25 17:14:36 +0200

    deploy API

--- auco/src/API.js
+++ auco/src/API.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:8081`,
+  // Developing
+  //baseURL: `http://localhost:8081`,
+
+  // Deploying
+  baseURL: `http://auco.gti-ia.upv.es/api`,
   rejectUnauthorized: false
 });
