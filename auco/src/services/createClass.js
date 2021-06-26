b15b0da891fb6d3a6408ddd814751d676bd67905
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-06-26 13:12:21 +0200

    Download excel file on response create class

--- auco/src/services/createClass.js
+++ auco/src/services/createClass.js
@@ -1,12 +1,15 @@
 // createClass.js
 import API from '../API';
+var FileSaver = require('file-saver');
 
 export async function postClass(newClass) {
     console.log("postClass()");
-    const token = await API.post(`/class/create`, newClass)
+    const token = await API.post(`/class/create`, newClass, { responseType: 'arraybuffer' })
         .then(res => {
             console.log("Success creating class!");
-            return res.data;
+            var blob = new Blob([res.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+            FileSaver.saveAs(blob, 'usuarios-estudiantes.xlsx');
+            return true;
         })
         .catch((err) => {
             console.log("Error in creation of class!");
