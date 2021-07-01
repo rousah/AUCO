eb5e67f7550e28b66595b06b8d2aae747e6f0fd9
Emilia Rosa van der Heide <emiliarosavdh@gmail.com>
2021-07-01 15:28:11 +0200

    Trying to fix bootstrap reboot bug in build

--- auco/src/index.js
+++ auco/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "../src/main.scss";
 import './index.css';
